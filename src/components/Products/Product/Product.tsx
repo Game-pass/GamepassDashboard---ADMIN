b0fa28c05f5a1467d3f6a9a02c3bf667b55e3b71
Dadieng <alfreddadieng18@outlook.com>
2024-07-17 00:08:35 +0100

    Your commit message

---
+++ src/components/Products/Product/Product.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { Button, Card, Chip } from "@material-tailwind/react";
+import { AiOutlineEdit } from "react-icons/ai";
+import { useMaterialTailwindController } from "context";
+import EditModal from "../EditModal/EditModal";
+import { useMediaQuery } from "react-responsive";
+
+const Product = ({ item }: any) => {
+  const [detailModal, setDetailModal] = useState(false);
+  const [controller]: any = useMaterialTailwindController();
+  const { sidenavColor } = controller;
+  const isSm = useMediaQuery({ query: "(min-width: 700px)" });
+
+  function handleOpen() {
+    setDetailModal((prev) => !prev);
+  }
+
+  return (
+    <Card
+      className="sm:w-[250px] w-[96%] h-full shadow-lg mx-2 mb-4 bg-blue-gray-50"
+      key={item.id}
+    >
+      <div className="w-full h-full rounded-t-lg">
+        <img
+          src={`/images/products/awards/${item.image}.png`}
+          alt="product"
+          loading="lazy"
+        />
+      </div>
+      <div className="flex justify-between gap-1 mx-4 my-1 font-medium text-blue-gray-400 text-sm">
+        <p>Type: {item.type}</p>
+        <p>Value: {item.value}</p>
+      </div>
+      <div className="text-center flex justify-between m-3">
+        <Chip
+          color={item.active ? "green" : "red"}
+          variant="gradient"
+          value={item.active ? "Active" : "DeActive"}
+          className="text-[10px]"
+        />
+        <Button
+          className="min-w-[100px] flex justify-center gap-2"
+          color={sidenavColor}
+          variant="outlined"
+          size="sm"
+          onClick={handleOpen}
+        >
+          <AiOutlineEdit size={15} />
+          <p className="text-xs font-normal capitalize">Edit Product</p>
+        </Button>
+      </div>
+
+      {detailModal && (
+        <EditModal
+          open={detailModal}
+          size={isSm ? "lg" : "xxl"}
+          handleOpen={handleOpen}
+          data={item}
+        />
+      )}
+    </Card>
+  );
+};
+
+export default Product;
---
+++ src/layouts/PanelLayout/PanelLayout.tsx
@@ -0,0 +1,23 @@
+import { Outlet } from "react-router-dom";
+import { Configurator } from "../../components";
+import Header from "./Header/Header";
+import Sidebar from "./Sidebar/Sidebar";
+import { useMaterialTailwindController } from "context";
+
+const PanelLayout = () => {
+  const [controller]: any = useMaterialTailwindController();
+  const { sidenavType } = controller;
+
+  return (
+    <div className={`min-h-screen ${sidenavType === "dark" ? "bg-[#111]" : "bg-[#e9e9e9]"}`}>
+      <Sidebar />
+      <div className="p-4 xl:ml-80">
+        <Configurator />
+        <Header />
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default PanelLayout;
